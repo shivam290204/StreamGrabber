9838e9937eccf554105678cb0b157422ab752f4d
shivam2004tiwri <shivam2004tiwri@gmail.com>
2026-07-23 21:57:02 +0530

    Allow Vercel frontend CORS

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ connectDB();
 
 // CORS configuration - allow Vite frontend access
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://stream-grabber.vercel.app'],
   credentials: true
 }));
 
